db248c0e4a9e45450f926891dc3366af1e1cdad3
Qossim Afolayan <qossimafolayan@gmail.com>
2022-05-23 15:17:16 +0100

    modified properties of an Image

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,1 +1,15 @@
-import Image
+import image
+p = image.Pixel(45, 76, 200)
+print(p.getRed())
+p.setRed(66)
+print(p.getRed())
+p.setBlue(p.getGreen())
+print(p.getGreen(), p.getBlue())
+
+# img = image.Image("first.gif")
+
+# print(img.getWidth())
+# print(img.getHeight())
+
+# p = img.getPixel(45, 55)
+# print(p.getRed(), p.getGreen(), p.getBlue())
